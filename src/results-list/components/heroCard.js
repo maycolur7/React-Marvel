c8ecd1f8ac3abf2cc545f11a0c4ac5d7dc19f443
Maycol Uribe Romero <maycolur7@gmail.com>
2018-07-20 10:34:07 +0200

    hero card fixed

--- src/results-list/components/heroCard.js
+++ src/results-list/components/heroCard.js
@@ -4,11 +4,13 @@ import './heroCard.css'
 function HeroCard (props) {
   return (
     <div className="card">
-      <img className="card-img-top"  alt="Card image cap" />
+      <img className="card-img-top" src={`${props.thumbnail.path}.${props.thumbnail.extension}`}  alt={props.name} />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
+          <h5 className="card-title">{props.name}</h5>
+          {
+            props.description &&
+              <p>{props.description}</p>            
+          }
           <a href="#" className="btn btn-danger">Go somewhere</a>
         </div>
     </div>
